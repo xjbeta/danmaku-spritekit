67a66e40ff49b18f968962637b9abd54ceadcbcc
xjbeta <xjbeta.tiny@gmail.com>
2022-01-03 20:00:48 +0800

    Initial Commit

---
+++ Danmaku/DanmakuApp.swift
@@ -0,0 +1,17 @@
+//
+//  DanmakuApp.swift
+//  Danmaku
+//
+//  Created by xjbeta on 1/3/22.
+//
+
+import SwiftUI
+
+@main
+struct DanmakuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
